f9ae1dc8eff1799434983969fc142a9e587f7b9f
vinichagastrexx <vinicius@trexx.com.br>
2023-06-18 14:40:45 -0300

    refactor: connect services with postgresql

--- api/routes/itemRoutes.js
+++ api/routes/itemRoutes.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const ItemController = require('../src/controllers/itemController');
+const ItemController = require("../src/controllers/itemController");
 
-router.post('/create-item', ItemController.createItem);
-router.post('/add-to-pool/:nftId', ItemController.addToPool);
-router.get('/get-in-pool/:owner',  ItemController.getItemsInPoolByUser);
-router.get('/get-rented/:owner',  ItemController.getItemsRentedByUser);
-router.get('/get-owned/:owner',  ItemController.getItemsOwnedByUser);
-
+router.post("/create-item", ItemController.createItem);
+router.post("/add-to-pool/:nftId", ItemController.addToPool);
+router.get("/get-in-pool/:owner", ItemController.getItemsInPoolByUser);
+router.get("/get-rented/:owner", ItemController.getItemsRentedByUser);
+router.get("/get-owned/:owner", ItemController.getItemsOwnedByUser);
 
 module.exports = router;
--- api/routes/poolRoutes.js
+++ api/routes/poolRoutes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const PoolController = require("../src/controllers/poolController")
+const PoolController = require("../src/controllers/poolController");
 
-router.get('/get-all', PoolController.getAll);
-router.get('/:poolId', PoolController.getById);
-router.post('/create-pool', PoolController.createPool);
+router.get("/get-all", PoolController.getAll);
+router.get("/:poolId", PoolController.getById);
+router.post("/create-pool", PoolController.createPool);
 
 module.exports = router;
