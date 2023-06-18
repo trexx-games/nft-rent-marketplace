cb67ed021ca31f953b0572aed7a681e7c16a3efa
vinichagastrexx <vinicius@trexx.com.br>
2023-06-18 14:41:24 -0300

    chore: add hardhat-waffle

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,4 +1,21 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle")
+
+const env = {
+  AVALANCHE_PRIVATE_KEY: "9bcdcecec956d01178a1320ff0cbe362eb59f8fe711642703b7419966320df52",
+  FUJI_RPC_URL:
+    "https://avalanche-fuji.rpc.thirdweb.com/ed043a51ae23b0db3873f5a38b77ab28175fa496f15d3c53cf70401be89b622a",
+}
+
 module.exports = {
+  defaultNetwork: "fuji",
+  networks: {
+    hardhat: {},
+    fuji: {
+      url: env.FUJI_RPC_URL || "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [`0x${env.AVALANCHE_PRIVATE_KEY}`],
+      gasPrice: 225000000000,
+      chainId: 43113,
+    },
+  },
   solidity: "0.8.18",
-};
+}
