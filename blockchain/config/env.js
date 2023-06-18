7a5f03254e3dfca46832c157cc311cc1d7f34c0f
vinichagastrexx <vinicius@trexx.com.br>
2023-06-18 14:42:14 -0300

    fix: prettier

--- blockchain/config/env.js
+++ blockchain/config/env.js
@@ -11,14 +11,5 @@ module.exports = {
   vrfCoordinatorContractAddress: process.env.VRF_COORDINATOR_CONTRACT ?? "",
   vrfSubId: Number(process.env.VRF_SUB_ID) ?? 0,
   cfSubId: Number(process.env.CF_SUB_ID) ?? 0,
-  cfOracleAddress: process.env.CF_ORACLE_ADDRESS ?? "",
-  sxtPvKey: process.env.SXT_PV_KEY ?? "",
-  sxtURL: process.env.SXT_URL ?? "",
-  sxtPubKey: process.env.SXT_PUB_KEY ?? "",
-  sxtUserId: process.env.SXT_USER_ID ?? "",
-  sxtCLIPath: process.env.SXT_CLI_PATH ?? "",
-  sxtOrgCode: process.env.SXT_ORG_CODE ?? "",
-  biscuitItems: process.env.BISCUIT_ITEMS ?? "",
-  biscuitRents: process.env.BISCUIT_RENTS ?? "",
-  biscuitPools: process.env.BISCUIT_POOLS ?? ""
+  cfOracleAddress: process.env.CF_ORACLE_ADDRESS ?? ""
 }
