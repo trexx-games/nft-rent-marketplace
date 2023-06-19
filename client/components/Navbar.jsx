c1a7416b6684b511b42926ee2039359ec20ad776
vinichagastrexx <vinicius@trexx.com.br>
2023-06-19 11:01:03 -0300

    update navbar to remove mention of bbg and remove recommendation

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -19,35 +19,11 @@ import {
   ModalFooter,
   Button,
 } from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { darken } from '@chakra-ui/theme-tools';
-import { URLS } from '../config/urls';
+import {  useState } from 'react';
 
 export function Navbar() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [recommendation, setRecommendation] = useState(null);
   const [activeTab, setActiveTab] = useState('');
   const address = useAddress();
-
-  useEffect(() => {
-    const fetchRecommendations = async () => {
-      if (address) {
-        try {
-          const response = await fetch(
-            `${URLS.RECOMMENDATIONS}/${address}`,
-          );
-          const data = await response.json();
-          if (data.recommendations.length > 0) {
-            setRecommendation(data.recommendations[0]);
-            onOpen();
-          }
-        } catch (error) {
-          console.error('Failed to fetch recommendations:', error);
-        }
-      }
-    };
-    fetchRecommendations();
-  }, [address]);
 
   return (
     <Box maxW={'85%'} m={'auto'} py={'14px'} px={'35px'}>
@@ -137,58 +113,6 @@ export function Navbar() {
           )}
         </Flex>
       </Flex>
-
-      {recommendation !== null && (
-        <Modal isOpen={isOpen} onClose={onClose} motionPreset="slideInBottom">
-          <ModalOverlay />
-          <ModalContent borderRadius={'15px'} padding={4}>
-            <ModalHeader
-              fontSize={28}
-              fontWeight="bold"
-              fontFamily={'Bayon'}
-              mb={1}
-            >
-              Welcome back Gamer!
-            </ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <Text fontSize="lg" fontFamily={'Bayon'} mb={1}>
-                We have analyzed your profile and found some personalized
-                recommendations for you:
-              </Text>
-              <Text
-                marginTop={4}
-                fontSize="m"
-                fontFamily={'Big Shoulders Text'}
-                mb={1}
-              >
-                {recommendation.text}
-              </Text>
-            </ModalBody>
-            <ModalFooter justifyContent={'center'}>
-              <Button
-                _hover={{
-                  bg: darken('#FBAA0B', 15),
-                  transition: 'background-color 0.2s',
-                }}
-                _active={{
-                  transform: 'scale(0.98)',
-                }}
-                backgroundColor={'#FBAA0B'}
-                fontFamily={'Bayon'}
-                color={'white'}
-                mr={3}
-                as={NextLink}
-                letterSpacing={0.5}
-                href={recommendation.type === 3 ? '/inventory' : '/pools'}
-                onClick={onClose}
-              >
-                {recommendation.callToAction}
-              </Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      )}
     </Box>
   );
 }
