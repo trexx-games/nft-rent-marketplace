c5d36804e8b0393157801b482851d00ad72ae44f
vinichagastrexx <vinicius@trexx.com.br>
2023-06-19 10:58:47 -0300

    feat: adapt Rentservice

--- api/src/controllers/rentController.js
+++ api/src/controllers/rentController.js
@@ -1,32 +1,53 @@
-const RentService = require('../services/rentService');
-const ItemService = require('../services/itemService');
+const RentService = require("../services/rentService");
 
 class RentController {
   static async startRent(req, res) {
-    const { rentId, nftId, poolId, rentee, price, expirationDate, initDate, owner } = req.body;
-    const accessToken = req.accessToken;
-    await RentService.startRent({ accessToken, rentId, nftId, poolId, rentee, price, expirationDate, initDate, owner });
-    res.status(201).json({ message: 'Rent started successfully' });
+    const {
+      rentId,
+      nftId,
+      poolId,
+      rentee,
+      price,
+      expirationDate,
+      initDate,
+      owner,
+    } = req.body;
+    
+    await RentService.startRent({
+      rentId,
+      nftId,
+      poolId,
+      rentee,
+      price,
+      expirationDate,
+      initDate,
+      owner,
+    });
+    res.status(201).json({ message: "Rent started successfully" });
   }
 
   static async finishRent(req, res) {
     const { rentId, finishDate } = req.body;
-    const accessToken = req.accessToken;
-    await RentService.finishRent({ accessToken, rentId, finishDate });
+    
+    await RentService.finishRent({ rentId, finishDate });
 
-    res.status(201).json({ message: 'Rent finished successfully' });
+    res.status(201).json({ message: "Rent finished successfully" });
   }
 
   static async getActiveByRentee(req, res) {
-    const accessToken = req.accessToken;
-    const rents = await RentService.getActiveByRentee({ accessToken, rentee: req.params.rentee });
+    
+    const rents = await RentService.getActiveByRentee({
+      rentee: req.params.rentee,
+    });
 
     res.status(200).json({ rents });
   }
 
   static async getAllByRentee(req, res) {
-    const accessToken = req.accessToken;
-    const rents = await RentService.getAllByRentee({ accessToken, rentee: req.params.rentee });
+    
+    const rents = await RentService.getAllByRentee({
+      rentee: req.params.rentee,
+    });
 
     res.status(200).json({ rents });
   }
--- api/src/services/rentService.js
+++ api/src/services/rentService.js
@@ -1,95 +1,117 @@
-const ItemService = require('./itemService');
-const SxTApi = require('./sxtApi');
-const env = require('../../config/env')
-const { logRent } = require('../../helpers/logRents')
-const pool = require('../../helpers/pgConnection')
+const ItemService = require("./itemService");
+const pool = require("../../helpers/pgConnection");
 
 class RentService {
-  static async getRentsLastDay() {
+  static async startRent({
+    rentId,
+    nftId,
+    poolId,
+    rentee,
+    price,
+    expirationDate,
+    initDate,
+    owner,
+  }) {
     const query = `
-      SELECT COUNT(*)
-      FROM rents
-      WHERE date > NOW() - INTERVAL '24 HOURS';
+      INSERT INTO rents 
+      (id, initDate, expirationDate, finishDate, rentPrice, rentee, poolId, nftId, owner) 
+      VALUES ($1, $2, $3, NULL, $4, $5, $6, $7, $8);
     `;
 
     try {
-      const result = await pool.query(query);
-      return result.rows[0].count;
+      await pool.query(query, [
+        rentId,
+        initDate,
+        expirationDate,
+        price,
+        rentee,
+        poolId,
+        nftId,
+        owner,
+      ]);
+      await ItemService.rentItem({ nftId, rentee });
     } catch (error) {
-      console.error('Error getting rent count for the last 24 hours: ', error.stack);
+      console.error("Error starting rent: ", error.stack);
     }
   }
 
-  static async startRent({ accessToken, rentId, nftId, poolId, rentee, price, expirationDate, initDate, owner }) {
-    const resourceId = `${env.sxtSchema}.RENTS`
-    const sqlText = `INSERT INTO ${env.sxtSchema}.RENTS (Id, initDate, expirationDate, finishDate, rentPrice, rentee, poolId, nftId, owner) VALUES (${rentId}, '${initDate}', '${expirationDate}', null, ${price}, '${rentee}', ${poolId}, ${nftId}, '${owner}');`;
-    const response = await SxTApi.dml({
-      resourceId,
-      sqlText,
-      accessToken,
-    });
-    await ItemService.rentItem({ accessToken, nftId, rentee })
-    await logRent(rentId, price, initDate);
-    return response;
+  static async getRentById({ rentId }) {
+    const query = `
+      SELECT * 
+      FROM rents 
+      WHERE id = $1;
+    `;
+
+    try {
+      const result = await pool.query(query, [rentId]);
+      return result.rows[0];
+    } catch (error) {
+      console.error("Error getting rent by id: ", error.stack);
+    }
   }
 
-  static async getRentById({ accessToken, rentId }) {
-    const resourceId = `${env.sxtSchema}.RENTS`
-    const sqlText = `SELECT * FROM ${env.sxtSchema}.RENTS WHERE Id = ${rentId};`;
-    const response = await SxTApi.dql({
-      resourceId,
-      sqlText,
-      accessToken,
-    });
+  static async finishRent({ rentId, finishDate }) {
+    const query = `
+      UPDATE rents 
+      SET finishDate = $1 
+      WHERE id = $2;
+    `;
 
-    return response[0];
+    try {
+      await pool.query(query, [finishDate, rentId]);
+      const rent = await RentService.getRentById(rentId);
+      const rentedNftId = rent.nft_id;
+      await ItemService.finishRent({ nftId: rentedNftId });
+    } catch (error) {
+      console.error("Error finishing rent: ", error.stack);
+    }
   }
 
-  static async finishRent({ accessToken, rentId, finishDate }) {
-    const resourceId = `${env.sxtSchema}.RENTS`
-    const sqlText = `UPDATE ${env.sxtSchema}.RENTS SET finishDate = '${finishDate}' WHERE Id = ${rentId};`;
-    const response = await SxTApi.dml({
-      resourceId,
-      sqlText,
-      accessToken,
-    });
-    const rent = await RentService.getRentById({ accessToken, rentId });
-    const rentedNftId = rent.NFTID;
-    await ItemService.finishRent({ accessToken, nftId: rentedNftId });
-    return response;
+  static async getActiveByRentee({ rentee }) {
+    const query = `
+      SELECT rents.*, categories.gameid
+      FROM rents
+      INNER JOIN pools ON rents.poolid = pools.categoryid
+      INNER JOIN categories ON pools.categoryid = categories.id
+      WHERE rents.rentee = $1 AND rents.finishDate IS NULL;
+    `;
+
+    try {
+      const result = await pool.query(query, [rentee]);
+      return result.rows;
+    } catch (error) {
+      console.error("Error getting active rents by rentee: ", error.stack);
+    }
   }
 
-  static async getActiveByRentee({ accessToken, rentee }) {
-    const resourceId = `${env.sxtSchema}.RENTS`
-    const sqlText = `SELECT * FROM ${env.sxtSchema}.RENTS WHERE ${env.sxtSchema}.RENTS.RENTEE = '${rentee}' AND ${env.sxtSchema}.RENTS.FINISHDATE IS NULL;`;
-    const response = await SxTApi.dql({
-      resourceId,
-      sqlText,
-      accessToken,
-    });
-    return response;
+  static async getActiveByOwner({ owner }) {
+    const query = `
+      SELECT * 
+      FROM rents 
+      WHERE owner = $1 AND finishDate IS NULL;
+    `;
+
+    try {
+      const result = await pool.query(query, [owner]);
+      return result.rows;
+    } catch (error) {
+      console.error("Error getting active rents by owner: ", error.stack);
+    }
   }
 
-  static async getActiveByOwner({ accessToken, owner }) {
-    const resourceId = `${env.sxtSchema}.RENTS`
-    const sqlText = `SELECT * FROM ${env.sxtSchema}.RENTS WHERE ${env.sxtSchema}.RENTS.OWNER = '${owner}' AND ${env.sxtSchema}.RENTS.FINISHDATE IS NULL;`;
-    const response = await SxTApi.dql({
-      resourceId,
-      sqlText,
-      accessToken,
-    });
-    return response;
-  }
+  static async getAllByRentee({ rentee }) {
+    const query = `
+      SELECT * 
+      FROM rents 
+      WHERE rentee = $1;
+    `;
 
-  static async getAllByRentee({ accessToken, rentee }) {
-    const resourceId = `${env.sxtSchema}.RENTS`
-    const sqlText = `SELECT * FROM ${env.sxtSchema}.RENTS WHERE ${env.sxtSchema}.RENTS.RENTEE = '${rentee}';`;
-    const response = await SxTApi.dql({
-      resourceId,
-      sqlText,
-      accessToken,
-    });
-    return response;
+    try {
+      const result = await pool.query(query, [rentee]);
+      return result.rows;
+    } catch (error) {
+      console.error("Error getting all rents by rentee: ", error.stack);
+    }
   }
 }
 
