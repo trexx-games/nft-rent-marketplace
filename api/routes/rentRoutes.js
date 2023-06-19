f530e49af3def8f53010511217119139bf95f35e
vinichagastrexx <vinicius@trexx.com.br>
2023-06-19 10:58:56 -0300

    prettier fix

--- api/routes/rentRoutes.js
+++ api/routes/rentRoutes.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const RentController = require('../src/controllers/rentController');
+const RentController = require("../src/controllers/rentController");
 
-router.post('/start-rent', RentController.startRent);
-router.get('/get-active-by-rentee/:rentee', RentController.getActiveByRentee);
-router.get('/get-last-day', RentController.getRentsLastDay);
-router.get('/get-all-by-rentee/:rentee', RentController.getAllByRentee);
-router.post('/finish-rent', RentController.finishRent);
+router.post("/start-rent", RentController.startRent);
+router.get("/get-active-by-rentee/:rentee", RentController.getActiveByRentee);
+router.get("/get-last-day", RentController.getRentsLastDay);
+router.get("/get-all-by-rentee/:rentee", RentController.getAllByRentee);
+router.post("/finish-rent", RentController.finishRent);
 
 module.exports = router;
