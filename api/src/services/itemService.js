664a863b1c756b3f68a830c58437f658ed09b2e7
Vini Chagas <133237806+vinichagastrexx@users.noreply.github.com>
2023-07-11 12:26:32 -0300

    Update itemService.js

--- api/src/services/itemService.js
+++ api/src/services/itemService.js
@@ -7,7 +7,11 @@ class ItemService {
     nftId = Number(nftId)
     return await this.itemModel.getItemByNftId(nftId);
   }
-
+  
+  async getById(itemId) {
+    return await this.itemModel.getById(itemId);
+  }
+  
   async createItem(itemData) {
     return await this.itemModel.createItem(itemData);
   }
@@ -33,12 +37,12 @@ class ItemService {
     return await this.itemModel.addToPool(itemId);
   }
 
-  async getItemsInPoolByUser(owner) {
-    return await this.itemModel.getItemsInPoolByUser(owner);
+  async getItemsInPoolByUser(ownerAddress) {
+    return await this.itemModel.getItemsInPoolByUser(ownerAddress);
   }
 
-  async getItemsRentedByUser(owner) {
-    return await this.itemModel.getItemsRentedByUser(owner);
+  async getItemsRentedByUser(ownerAddress) {
+    return await this.itemModel.getItemsRentedByUser(ownerAddress);
   }
 }
 
