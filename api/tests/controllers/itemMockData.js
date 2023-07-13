617d528b812e04f985c9f70a5ffa755bc3eb38f6
vinichagastrexx <vinicius@trexx.com.br>
2023-07-13 15:41:55 -0300

    testing CI flow

--- api/tests/controllers/itemMockData.js
+++ api/tests/controllers/itemMockData.js
@@ -1,52 +1,42 @@
-const mockNewItem = { 
-  id: 123, 
-  nftId: 456, 
-  categoryId: 123, 
-  ownerAddress: 'Ana123',
-  gameId: 123, 
-  nftContractAddress: 'qwe', 
-  rarityId: 123, 
-  blockchainId: 123,
-};
 
-const resMockNewItem = { 
-  id: 456, 
-  nftId: 123, 
-  categoryId: 123, 
+const resMockNewItem = {
+  id: 456,
+  nftId: 123,
+  categoryId: 123,
   ownerAddress: 'Ana123',
   renteeAdress: null,
-  isInPool: false, 
-  gameId: 123, 
-  nftContractAddress: 'asd', 
-  rarityId: 123, 
+  isInPool: false,
+  gameId: 123,
+  nftContractAddress: 'asd',
+  rarityId: 123,
   blockchainId: 456,
   isRented: false,
 };
 
-const mockItemIsRented = { 
-  id: 456, 
-  nftId: 123, 
-  categoryId: 123, 
+const mockItemIsRented = {
+  id: 456,
+  nftId: 123,
+  categoryId: 123,
   ownerAddress: 'Ana123',
   renteeAdress: 'Bia456',
-  isInPool: true, 
-  gameId: 123, 
-  nftContractAddress: 'asd', 
-  rarityId: 123, 
+  isInPool: true,
+  gameId: 123,
+  nftContractAddress: 'asd',
+  rarityId: 123,
   blockchainId: 456,
   isRented: true,
 };
 
-const mockItemIsInPool = { 
-  id: 123, 
-  nftId: 456, 
-  categoryId: 123, 
+const mockItemIsInPool = {
+  id: 123,
+  nftId: 456,
+  categoryId: 123,
   ownerAddress: 'Ana123',
   renteeAdress: 'Bia456',
-  isInPool: true, 
-  gameId: 123, 
-  nftContractAddress: 'qwe', 
-  rarityId: 123, 
+  isInPool: true,
+  gameId: 123,
+  nftContractAddress: 'qwe',
+  rarityId: 123,
   blockchainId: 123,
   isRented: false,
 };
